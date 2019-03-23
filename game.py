57b484e42f0b51d49a4185ad827890dd2955c1eb
rotzbouf <rotzbouf@gmail.com>
2019-03-23 22:05:48 +0100

    geändert:       game.py

--- game.py
+++ game.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python2
 import libtcodpy as libtcod
 
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
 class Object:
     # generic object class
     def __init__(self, x, y, char, color):
@@ -9,17 +22,22 @@ class Object:
         self.char = char
         self.color = color
     def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
+        if not map[self.x + dx][self.y + dy].blocked:
+            self.x += dx
+            self.y += dy
     def draw(self):
         libtcod.console_set_default_foreground(con, self.color)
         libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
     def clear(self):
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
-LIMIT_FPS = 20
+
+class Tile:
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
 
 def handle_keys():
     global playerx, playery
@@ -38,18 +56,66 @@ def handle_keys():
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         player.move(1, 0)
 
+def create_room(room):
+    global map
+    for x in range(room.x1 + 1, room.x2):
+        for y in range (room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+    global map
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def make_map():
+    global map
+    map = [[ Tile(True)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+    create_h_tunnel(25, 55, 23)
+
+def render_all():
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET )
+            else:
+                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET )
+    for object in objects:
+        object.draw()
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'DungeonCrawler v0.1', False)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
 
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
+make_map()
+player.x = 25
+player.y = 23
+
 while not libtcod.console_is_window_closed():
-    for object in objects:
-        object.draw()
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    render_all()
     libtcod.console_flush()
     for object in objects:
         object.clear() 
