259aa99443a691f38e338cf543184aa48a2f724c
rotzbouf <rotzbouf@gmail.com>
2019-03-27 20:41:18 +0100

    inventory

--- render_functions.py
+++ render_functions.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from game_states import GameStates
+from menus import inventory_menu
 
 class RenderOrder(Enum):
     CORPSE = 1
@@ -32,7 +34,7 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
     panel.draw_str(x_centered, y, text, fg=string_color, bg=None)
 
 def render_all(con, panel, entities, player, game_map, fov_recompute, root_console, message_log, screen_width,
-               screen_height, bar_width, panel_height, panel_y, mouse_coordinates, colors):
+               screen_height, bar_width, panel_height, panel_y, mouse_coordinates, colors, game_state)
     # Draw all the tiles in the game map
     if fov_recompute:
         for x, y in game_map:
@@ -72,6 +74,14 @@ def render_all(con, panel, entities, player, game_map, fov_recompute, root_conso
     panel.draw_str(1, 0, get_names_under_mouse(mouse_coordinates, entities, game_map))
 
     root_console.blit(panel, 0, panel_y, screen_width, panel_height, 0, 0)
+
+    if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        if game_state == GameStates.SHOW_INVENTORY:
+            inventory_title = 'Press the key next to an item to use it, or Esc to cancel.\n'
+        else:
+            inventory_title = 'Press the key next to an item to drop it, or Esc to cancel.\n'
+
+        inventory_menu(con, root_console, inventory_title, player.inventory, 50, screen_width, screen_height)
 
 
 def clear_all(con, entities):
