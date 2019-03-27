0504c6963e8233e8c148cb29de64a3d76127fd47
rotzbouf <rotzbouf@gmail.com>
2019-03-27 19:38:34 +0100

    simple ui

--- engine.py
+++ engine.py
@@ -5,6 +5,7 @@ import tdl
 from components.fighter import Fighter
 from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
+from game_messages import MessageLog
 from game_states import GameStates
 from input_handlers import handle_keys
 from map_utils import GameMap, make_map
@@ -14,8 +15,17 @@ from render_functions import clear_all, render_all, RenderOrder
 def main():
     screen_width = 80
     screen_height = 50
+
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -35,7 +45,13 @@ def main():
         # Monster / Item Colors
         'desaturated_green': (63, 127, 63),
         'darker_green': (0, 127, 0),
-        'dark_red': (191, 0, 0)
+        'dark_red': (191, 0, 0),
+        'white': (255, 255, 255),
+        'black': (0, 0, 0),
+        'red': (255, 0, 0),
+        'orange': (255, 127, 0),
+        'light_red': (255, 114, 114),
+        'darker_red': (127, 0, 0)
     }
 
     fighter_component = Fighter(hp=30, defense=2, power=5)
@@ -46,18 +62,24 @@ def main():
 
     root_console = tdl.init(screen_width, screen_height, title='ASCII Hero v0.2')
     con = tdl.Console(screen_width, screen_height)
+    panel = tdl.Console(screen_width, panel_height)
 
     game_map = GameMap(map_width, map_height)
     make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, colors)
 
     fov_recompute = True
 
+    message_log = MessageLog(message_x, message_width, message_height)
+
+    mouse_coordinates = (0, 0)
+
     game_state = GameStates.PLAYERS_TURN
 
     while not tdl.event.is_window_closed():
         if fov_recompute:
             game_map.compute_fov(player.x, player.y, fov=fov_algorithm, radius=fov_radius, light_walls=fov_light_walls)
-        render_all(con, entities, player, game_map, fov_recompute, root_console, screen_width, screen_height, colors)
+        render_all(con, panel, entities, player, game_map, fov_recompute, root_console, message_log, screen_width,
+                   screen_height, bar_width, panel_height, panel_y, mouse_coordinates, colors)
         tdl.flush()
 
         clear_all(con, entities)
@@ -68,6 +90,8 @@ def main():
             if event.type == 'KEYDOWN':
                 user_input = event
                 break
+            elif event.type == 'MOUSEMOTION':
+                mouse_coordinates = event.cell
         else:
             user_input = None
 
@@ -108,7 +132,7 @@ def main():
             dead_entity = player_turn_result.get('dead')
 
             if message:
-                print(message)
+                message_log.add_message(message)
 
             if dead_entity:
                 if dead_entity == player:
@@ -116,7 +140,7 @@ def main():
                 else:
                     message = kill_monster(dead_entity, colors)
 
-                print(message)
+                message_log.add_message(message)
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
@@ -128,7 +152,7 @@ def main():
                         dead_entity = enemy_turn_result.get('dead')
 
                         if message:
-                            print(message)
+                            message_log.add_message(message)
 
                         if dead_entity:
                             if dead_entity == player:
@@ -136,7 +160,7 @@ def main():
                             else:
                                 message = kill_monster(dead_entity, colors)
 
-                            print(message)
+                            message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
                                 break
